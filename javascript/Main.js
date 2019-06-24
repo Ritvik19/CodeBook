0ae1f5737c1cc9f7a00465b1487990b697cc7480
Ritvik19 <rastogiritvik99@gmail.com>
2019-06-24 20:53:36 +0530

    Problem Link Added

--- javascript/Main.js
+++ javascript/Main.js
@@ -4,32 +4,29 @@ var clipboard = new ClipboardJS('.copy-button');
 
 function snackbarDisplay()
 {
-  // Get the snackbar DIV
   var x = document.getElementById("snackbar");
-
-  // Add the "show" class to DIV
   x.className = "show";
-
-  // After 3 seconds, remove the show class from DIV
   setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
 
-// function clearCode(){
-//   document.getElementById('code').innerHTML = '';
-// }
 
 function loadCode()
 {
   var xhttp = new XMLHttpRequest();
-  var filepath = '../programs/'+document.getElementById('problem').value+'.py'
+  filename = document.getElementById('problem').value
+  var filepath = '../programs/'+filename+'.py'
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
      document.getElementById("code").innerHTML = this.responseText;
+     document.getElementById("prob-link").setAttribute('href', 'https://www.codechef.com/problems/'+filename)
      document.getElementById("code-btn").disabled = false;
+     document.getElementById("prob-btn").disabled = false;
     }
     else{
       document.getElementById("code").innerHTML = 'Ingredients for this recepie are not on the shelf';
+      document.getElementById("prob-link").setAttribute('href', '#')
       document.getElementById("code-btn").disabled = true;
+      document.getElementById("prob-btn").disabled = true;
     }
   };
   xhttp.open("GET", filepath, true);
