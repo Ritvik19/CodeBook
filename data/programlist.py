4776085b4c6b63c4cf4ca2056336fed05d5d4866
Ritvik19 <rastogiritvik99@gmail.com>
2019-09-14 18:29:01 +0530

    Redesigned

--- data/programlist.py
+++ data/programlist.py
@@ -5,15 +5,15 @@ codechef = []
 for folderName, subfolders, filenames in os.walk('CodeChef/'):
     for filename in filenames:
         codechef.append(filename[:-3])
-        
+
 hackerrank_python = []
 for folderName, subfolders, filenames in os.walk('HackerRank-Python//'):
     for filename in filenames:
         hackerrank_python.append(filename[:-3])
-        
+
 programList = pd.DataFrame({
     'Program': codechef+hackerrank_python,
-    'Category': ['CodeChef']*len(codechef)+['HackerRank Python']*len(hackerrank_python)
+    'Category': ['CodeChef']*len(codechef)+['HackerRank-Python']*len(hackerrank_python)
 }).sort_values('Program').reset_index(drop=True)
 print(len(programList))
-programList.to_json('ProgramList.json')       
+programList.to_json('ProgramList.json')
--- javascript/Main.js
+++ javascript/Main.js
@@ -9,35 +9,57 @@ function snackbarDisplay()
   setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
 
-function readRadio()
-{
-  var radio_btns = document.getElementsByClassName('with-gap');
-  for(i=0; i<radio_btns.length; i++)
-  {
-    if(radio_btns[i].checked)
-    {
-      return i
+function loadPrograms() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var dataObj = JSON.parse(this.responseText);
+      program_name = dataObj['Program']
+      platform = dataObj['Category']
+      var i =0;
+      HTMLcontent = ''
+      while(typeof program_name[i] !== "undefined")
+      {
+        HTMLcontent += '<li class="w3-padding-8 program '+platform[i]+'"><a href="/?q='+program_name[i]+'&p='+platform[i]+'"><span class="w3-large">'+program_name[i]+'</span><br><span>'+platform[i]+'</span></a></li>'
+        i ++;
+      }
+      document.getElementById("postlist").innerHTML += HTMLcontent;
     }
-  }
+  };
+  xhttp.open("GET", "data/ProgramList.json", true);
+  xhttp.send();
 }
 
-function loadCode()
+function listFilter(inputEl, listEl, element) {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById(inputEl);
+    filter = input.value.toUpperCase();
+    ul = document.getElementById(listEl);
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName(element)[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function loadCode(p, q)
 {
   var xhttp = new XMLHttpRequest();
-  filename = document.getElementById('problem').value
-  var site = readRadio();
-  console.log(site)
-  var filepath = '../programs/'+filename+'.py'
+  var filepath = '../data/'+p+'/'+q+'.py'
+  console.log(filepath)
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
      document.getElementById("code").innerHTML = this.responseText;
-     document.getElementById("prob-link").setAttribute('href', 'https://www.codechef.com/problems/'+filename)
      document.getElementById("code-btn").disabled = false;
      document.getElementById("prob-btn").disabled = false;
     }
     else{
       document.getElementById("code").innerHTML = 'Ingredients for this recepie are not on the shelf';
-      document.getElementById("prob-link").setAttribute('href', '#')
       document.getElementById("code-btn").disabled = true;
       document.getElementById("prob-btn").disabled = true;
     }
@@ -46,6 +68,12 @@ function loadCode()
   xhttp.send();
 }
 
-function loadDoc(){
-  window.open('../programs/'+document.getElementById('problem').value+'.py')
+function loadDoc(p, q){
+  window.open('../data/'+p+'/'+q+'.py')
 }
+
+loadPrograms();
+var params = new URLSearchParams(location.search);
+var q = params.get('q')
+var p = params.get('p')
+loadCode(p,q)
