a257c72200b8326385ae6a4d3301fd393d1feb28
Ritvik19 <rastogiritvik99@gmail.com>
2019-12-13 21:41:45 +0530

    UI Improvement

--- javascript/Main.js
+++ javascript/Main.js
@@ -2,33 +2,32 @@ document.getElementById("code").innerHTML = "Get your code here"
 document.getElementById("code-btn").disabled = true;
 var clipboard = new ClipboardJS('.copy-button');
 
-function snackbarDisplay()
-{
-  var x = document.getElementById("snackbar");
-  x.className = "show";
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+function snackbarDisplay() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function() { x.className = x.className.replace("show", ""); }, 3000);
 }
 
 function loadPrograms() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var dataObj = JSON.parse(this.responseText);
-      program_name = dataObj['Program']
-      platform = dataObj['Category']
-      extension = dataObj['Language']
-      var i =0;
-      HTMLcontent = ''
-      while(typeof program_name[i] !== "undefined")
-      {
-        HTMLcontent += '<li class="w3-padding-8 program" onclick="loadCode(\''+platform[i]+'\', \''+program_name[i]+'\', \''+extension[i]+'\')"><span class="w3-large">'+program_name[i]+'</span><br><span>'+platform[i]+'</span></li>'
-        i ++;
-      }
-      document.getElementById("postlist").innerHTML += HTMLcontent;
-    }
-  };
-  xhttp.open("GET", "data/ProgramList.json", true);
-  xhttp.send();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var dataObj = JSON.parse(this.responseText);
+            program_name = dataObj['Program']
+            platform = dataObj['Category']
+            extension = dataObj['Language']
+            var i = 0;
+            HTMLcontent = ''
+            while (typeof program_name[i] !== "undefined") {
+                HTMLcontent += '<li class="w3-padding-8 program" onclick="loadCode(\'' + platform[i] + '\', \'' + program_name[i] + '\', \'' + extension[i] + '\')"><span class="w3-large">' + program_name[i] + '</span><br><span>' + platform[i] + '</span></li>'
+                i++;
+            }
+            document.getElementById("postlist").innerHTML += HTMLcontent;
+            document.getElementsByClassName("badge")[0].innerHTML = i;
+        }
+    };
+    xhttp.open("GET", "data/ProgramList.json", true);
+    xhttp.send();
 }
 
 function listFilter(inputEl, listEl, element) {
@@ -48,34 +47,32 @@ function listFilter(inputEl, listEl, element) {
     }
 }
 
-function loadCode(p, q, e)
-{
-  var xhttp = new XMLHttpRequest();
-  var filepath = '../data/'+p+'/'+q+'.'+e
-  console.log(filepath)
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-     code = this.responseText;
-     code = code.replace(/</g,"&lt;")
-     code = code.replace(/>/g,"&gt;")
-     document.getElementById("code").innerHTML = code;
-     document.getElementById("code-btn").disabled = false;
-     document.getElementById("code-btn").setAttribute('onclick', "loadDoc('"+p+"', '"+q+"', '"+e+"')");
-     // document.getElementById("prob-btn").disabled = false;
-    }
-    else{
-      document.getElementById("code").innerHTML = 'Get your code here';
-      document.getElementById("code-btn").disabled = true;
-      // document.getElementById("prob-btn").disabled = true;
-    }
-  };
-  xhttp.open("GET", filepath, true);
-  xhttp.send();
+function loadCode(p, q, e) {
+    var xhttp = new XMLHttpRequest();
+    var filepath = '../data/' + p + '/' + q + '.' + e
+    console.log(filepath)
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            code = this.responseText;
+            code = code.replace(/</g, "&lt;")
+            code = code.replace(/>/g, "&gt;")
+            document.getElementById("code").innerHTML = code;
+            document.getElementById("code-btn").disabled = false;
+            document.getElementById("code-btn").setAttribute('onclick', "loadDoc('" + p + "', '" + q + "', '" + e + "')");
+            // document.getElementById("prob-btn").disabled = false;
+        } else {
+            document.getElementById("code").innerHTML = 'Get your code here';
+            document.getElementById("code-btn").disabled = true;
+            // document.getElementById("prob-btn").disabled = true;
+        }
+    };
+    xhttp.open("GET", filepath, true);
+    xhttp.send();
 }
 
-function loadDoc(p, q, e){
-  console.log(p+q+e)
-  window.open('../data/'+p+'/'+q+'.'+e)
+function loadDoc(p, q, e) {
+    console.log(p + q + e)
+    window.open('../data/' + p + '/' + q + '.' + e)
 }
 
 loadPrograms();
