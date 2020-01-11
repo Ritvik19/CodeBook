c0790ea56739148062fcb2832d8398ee2e806eaf
Ritvik19 <rastogiritvik99@gmail.com>
2020-01-11 23:49:03 +0530

    Commit 63

---
+++ data/HackerRank-ProblemSolving/Append and Delete.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the appendAndDelete function below.
+def appendAndDelete(s, t, k):
+    commonLength = 0
+    for _s, _t in zip(s,t):
+        if _s == _t:
+            commonLength += 1
+        else:
+            break
+    if len(s)+len(t)-2*commonLength > k:
+        return "No"
+    if (len(s)+len(t)-2*commonLength)%2==k%2:
+        return "Yes"
+    if (len(s)+len(t)-k)<0:
+        return "Yes"
+    return "No"
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    t = input()
+
+    k = int(input())
+
+    result = appendAndDelete(s, t, k)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
---
+++ data/HackerRank-ProblemSolving/Lisas Workbook.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the workbook function below.
+def workbook(n, k, arr):
+    num_special=0
+    cur_page=1
+
+    for i in range(len(arr)):
+        num_probs_in_chapter=arr[i]
+        num_full_pages, leftover_probs = divmod(num_probs_in_chapter, k)
+
+        total_pages = num_full_pages + ( 1 if leftover_probs else 0 )
+        problems_in_chapter=iter(range(1, arr[i]+1))
+
+        for _ in range(total_pages):
+            probs_on_page = [next(problems_in_chapter, None) for _ in range(k)]    
+            if cur_page in probs_on_page:
+                num_special+=1
+            cur_page+=1        
+    return num_special    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = workbook(n, k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
