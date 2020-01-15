bcfe3c6c83bce336b758b1640e539f0f4f918c91
Ritvik19 <rastogiritvik99@gmail.com>
2020-01-15 12:22:36 +0530

    Analysis

--- js/dashboard.js
+++ js/dashboard.js
@@ -51,7 +51,6 @@ function drawBarChart() {
     xhttp.open("GET", "../data/ProgramCounts.json", true);
     xhttp.send();
 }
-
 drawBarChart();
 
 function drawPieChart() {
@@ -84,4 +83,81 @@ function drawPieChart() {
     xhttp.open("GET", "../data/ProgramLanguage.json", true);
     xhttp.send();
 }
-drawPieChart()
+drawPieChart();
+
+function code() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var dataObj = JSON.parse(this.responseText);
+            console.log(dataObj)
+            document.getElementById('loc').innerHTML = dataObj['loc']
+            var ctx1 = document.getElementById('code').getContext('2d');
+            var myChart1 = new Chart(ctx1, {
+                type: 'horizontalBar',
+                data: {
+                    labels: ['Expressions', 'Decisions', 'Iterations'],
+                    datasets: [{
+                        label: '# occurences',
+                        data: [dataObj['expressions'], dataObj['decisions'], dataObj['for'] + dataObj['while']],
+                        backgroundColor: '#11174b',
+                        borderColor: '#b3ddcc',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    legend: {
+                        labels: {
+                            fontColor: "white",
+                        }
+                    },
+                    scales: {
+                        yAxes: [{
+                            gridLines: {
+                                color: "white"
+                            },
+                            ticks: {
+                                fontColor: "white",
+                                beginAtZero: true
+                            }
+                        }],
+                        xAxes: [{
+                            gridLines: {
+                                color: "white"
+                            },
+                            ticks: {
+                                fontColor: "white",
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+            var ctx2 = document.getElementById('loops').getContext('2d');
+            var myChart2 = new Chart(ctx2, {
+                type: 'pie',
+                data: {
+                    datasets: [{
+                        data: [dataObj['for'], dataObj['while']],
+                        backgroundColor: ["#2d5e9e", "#162065"],
+                    }],
+                    labels: ['For', 'While']
+                },
+                options: {
+                    legend: {
+                        labels: {
+                            fontColor: "white",
+                        }
+                    },
+                    scales: {}
+                }
+            });
+
+
+        }
+    };
+    xhttp.open("GET", "../data/ProgramAnalysis.json", true);
+    xhttp.send();
+}
+code();
--- run.py
+++ run.py
@@ -1,14 +1,31 @@
 import pandas as pd
-import os, sys
+import os, sys, json
 
 programList = []
+
+count_loc = 0
+count_for = 0
+count_while = 0
+count_if = 0
+count_asgnmt = 0
+
 
 for folderName, subfolders, filenames in os.walk('E:/Coding/CodeBook/data/'):
     for filename in filenames:
         *fname, extension = filename.split('.')
         fname = '.'.join(fname)
         dname = folderName.split('/')[-1]
+        if extension != 'json':
+            with open(f'{folderName}/{filename}') as f:
+                data = f.read()
+                count_loc += len(data.split('\n'))
+                count_asgnmt += data.count(' = ')
+                count_for += data.count('for')
+                count_while += data.count(('while'))
+                count_if += data.count('if')
+                
         programList.append((fname, extension, dname))
+programList.pop(0)
 programList.pop(0)
 programList.pop(0)
 programList.pop(0)
@@ -20,6 +37,10 @@ print(programList['Category'].value_counts())
 programList['Category'].value_counts().to_json('data/ProgramCounts.json')
 print(programList['Language'].value_counts())
 programList['Language'].value_counts().to_json('data/ProgramLanguage.json')
+
+json.dump(({'loc': count_loc, 'expressions': count_asgnmt, 'for': count_for,
+            'while': count_while, 'decisions': count_if}), open('data/ProgramAnalysis.json', 'w'))
+
 os.system('surge')
 c = sys.argv[1]
 os.system('git add .')
