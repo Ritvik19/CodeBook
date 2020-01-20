3c3d301dcc61649752ed31f260765330197bde26
Ritvik19 <rastogiritvik99@gmail.com>
2020-01-20 22:38:32 +0530

    Commit 75

---
+++ data/HackerRank-ProblemSolving/Marcs Cakewalk.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the marcsCakewalk function below.
+
+
+def marcsCakewalk(calorie):
+    calorie.sort(reverse=True)
+    miles = sum([c*2**i for i, c in enumerate(calorie)])
+    return miles
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ data/HackerRank-ProblemSolving/Maximum Perimeter Triangle.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumPerimeterTriangle function below.
+
+
+def maximumPerimeterTriangle(sticks):
+    sticks.sort()
+    i = len(sticks)-3
+    while i >= 0 and (sticks[i] + sticks[i+1] <= sticks[i+2]):
+        i -= 1
+    if i >= 0:
+        return (sticks[i], sticks[i+1], sticks[i+2])
+    else:
+        return (-1,)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    sticks = list(map(int, input().rstrip().split()))
+
+    result = maximumPerimeterTriangle(sticks)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
