3700f5ebd5e2d04c66ad0c221ae623c8b75f1dc7
Ritvik19 <rastogiritvik99@gmail.com>
2020-01-23 23:56:44 +0530

    Commit 76

---
+++ data/HackerRank-ProblemSolving/Absolute Permutation.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the absolutePermutation function below.
+
+
+def absolutePermutation(n, k):
+    if k == 0:
+        return range(1, n+1)
+    elif (n/k) % 2 != 0:
+        return (-1, )
+    else:
+        add = True
+        perm = []
+
+        for i in range(1, n+1):
+            if add:
+                perm.append(i+k)
+
+            else:
+                perm.append(i-k)
+
+            if i % k == 0:
+                if add:
+                    add = False
+                else:
+                    add = True
+        return perm
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nk = input().split()
+
+        n = int(nk[0])
+
+        k = int(nk[1])
+
+        result = absolutePermutation(n, k)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
---
+++ data/HackerRank-ProblemSolving/Sherlock and Anagrams.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+# Complete the sherlockAndAnagrams function below.
+
+
+def sherlockAndAnagrams(s):
+    count = 0
+    for i in range(1, len(s)+1):
+        a = ["".join(sorted(s[j:j+i])) for j in range(len(s)-i+1)]
+        b = Counter(a)
+        for j in b:
+            count += b[j]*(b[j]-1)/2
+    return int(count)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = sherlockAndAnagrams(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
